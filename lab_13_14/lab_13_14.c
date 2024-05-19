aca6a8a15729d7fc16b1c7e330df5684ed41ed50
OstapHyshka <164002481+OstapHyshka@users.noreply.github.com>
2024-05-19 11:51:09 +0300

    Update lab_13_14.c

--- lab_13_14/lab_13_14.c
+++ lab_13_14/lab_13_14.c
@@ -1,1 +1,77 @@
+// Приклад 1
+#include<stdio.h>
+#include<math.h>
+double Geron (double a,double b,double c); /*оголошення функцiї- прототипу*/
+double Geron (double a, double b, double c) { /*реалізація функції*/
+double p; /* локальна змiнна */
+p= (a + b + c)/2;
+return sqrt(p*(p-a)*(p-b)*(p-c));/*обчислити вираз i повернути результат*/
+}
+int main ( ) 
+{ double u, v, w;
+double s;
+printf ("Vvedit storonu trikutnika"); /* у консолі введіть значення 2, 3, 4 */
+scanf ("%lf %lf %lf",&u, &v,&w);
+s=Geron(u,v,w); /*викликається функцiя Geron. До аргументiв a,b,c
+передаються значення змiнних u,v,w. Результат виклику присвоюється змiннiй s*/
+printf("Ploscha 1 trikutnika %lf\n",s);
+s=Geron (10.3,8.1,9.7); /* викликається функцiя Geron. До аргументiв a,b,c
+передаються константи */
+printf("Ploscha 2 trikutnika %lf\n",s);
+s=Geron(u+10.3,v+w,w*1.7); /* викликається функцiя Geron. До аргументiв
+передаються значення виразiв */
+printf("Ploscha 3 trikutnika %lf\n",s);
+return 0;
+} 
+-------------------------------------------------------------------------
+// Vvedit storonu trikutnika 12 15 32
+// Ploscha 1 trikutnika -nan
+// Ploscha 2 trikutnika 36.928095
+// Ploscha 3 trikutnika 520.216903
 
+// Приклад 2
+#include<stdio.h>
+#include<math.h>
+double fract(int,int);
+double factorial(int);
+int main( ) {
+ int m,n,s;
+ scanf ("%d %d",&m,&n);
+ printf ("%lf\n",fract(m+1,n+1));
+ return 0;
+}
+double fract(int x, int y) {
+ double t;
+ t=factorial(x+y)/(x*y);
+ return t;
+}
+double factorial (int n) {
+ int i;
+ double p;
+ p=1;
+ for(i=1;i<=n;++i)
+ p*=i;
+ return p;}
+-------------------------------------------------------------------------
+// 12 31
+// 287553415542019677530610066802792465856224534626041856.000000
+
+// Приклад 3
+#include <stdio.h>
+// Функція для друкування перших n символів рядка
+void print_first_n_characters(char *str, int n) {
+    for (int i = 0; i < n && str[i] != '\0'; i++) {
+        printf("%c", str[i]);
+    }
+    printf("\n");
+}
+int main() {
+    char str[] = "Hello, World!";
+    int n = 5; // Визначаємо кількість символів, які потрібно надрукувати
+
+    // Викликаємо функцію для друкування перших n символів рядка
+    print_first_n_characters(str, n);
+    return 0;
+}
+-------------------------------------------------------------------------
+// Hello
